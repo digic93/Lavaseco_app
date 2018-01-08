b419efc5d36e1c14bc25511fe7dce25b08b3af26
digic93 <digic93@gmail.com>
2018-01-08 13:36:22 -0500

    correccion de roles y creacion de usuarios

---
+++ src/LavasecoBundle/Form/RegistrationType.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace LavasecoBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
+/**
+ * Description of RegistrationType
+ *
+ * @author diego
+ */
+class RegistrationType extends AbstractType {
+
+    public function buildForm(FormBuilderInterface $builder, array $options) {
+        $builder->add('firstName')
+                ->add('lastName')
+                ->add('phoneNumber')
+                ->add('roles',  ChoiceType::class, 
+                        array('label' => 'Rol', 'required' => true,
+                            'choices' => array( 
+                                'Vendedor' => 'ROLE_CASHIER',
+                                'Operador' => 'ROLE_OPERATOR', 
+                                'Administrador' => 'ROLE_MANAGER'
+                            ), 
+                    'multiple' => true));
+    }
+
+    public function getParent() {
+        return 'FOS\UserBundle\Form\Type\RegistrationFormType';
+    }
+
+    public function getName() {
+        return 'Lavaseco_user_registration';
+    }
+
+}
